72e858a36cb2d5468aefa8bf47dbb24c940c20cb
paulo <henriquepaulo0606@gmail.com>
2023-03-25 14:19:57 -0300

    Atividade-byteBank-entendendo orientado objeto

---
+++ src/Conta.java
@@ -0,0 +1,6 @@
+public class Conta {
+    // Declarando os atributos da classe conta
+    public double saldo;
+    public int agencia;
+    public int numero;
+}
---
+++ src/CriarConta.java
@@ -0,0 +1,17 @@
+public class CriarConta {
+    public static void main(String[] args) {
+        // Instanciando e criando a Primeira Conta atraves da classe conta
+        Conta primeiraConta = new Conta();
+        // Atribuindo valor pra primeira conta
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+        // Criando segunda conta obs: primeira conta nao tem nada a ver com a segunda conta são objetos diferente
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 100;
+        // Printando o valor de cada conta na tela
+        System.out.println("A primeira conta tem " +primeiraConta.saldo);
+        System.out.println("A segunda conta tem " +segundaConta.saldo);
+    }
+}
+
+
